215997f2265dae18e38ee78ad19e3e777e4a1a12
unknown <sashabidardel@gmail.com>
2021-11-17 13:42:36 +0100

    modified app.ts and modified play_event.ts

--- src/ts/app.ts
+++ src/ts/app.ts
@@ -1,11 +1,12 @@
 import {newUser, renderList} from '../ts/create_user';
 import { usersArray } from './elements';
+import { play } from './play_event';
 
 function app() : any
 {
     
     newUser();
-
+    play();
 }
 
 app();
--- src/ts/play_event.ts
+++ src/ts/play_event.ts
@@ -1,12 +1,28 @@
 
-import {btnPlay, usersArray } from "./elements";
+import { renderList } from "./create_user";
+import {btnPlay, coinsSection, usersArray } from "./elements";
 
 export function play() : void
 {
 
     btnPlay.addEventListener('click',() => {
         if(usersArray.length > 1) {
-            
+            let num=Math.floor(Math.random()*usersArray.length);
+           
+            let looser=usersArray[num];
+            //console.log(looser);
+            console.log(num);
+            usersArray.splice(num,1);
+           
+            //coinsSection.innerHTML=`<div>El jugador ${looser} ha sido eliminado</div>`;
+            alert ("El jugador" + looser + "ha sido eliminado");
+            //console.log(usersArray[num]);
+            renderList();
+            if (usersArray.length== 1){
+                let mensaje=document.getElementById('coin');
+                mensaje.innerHTML=`<div>El ganador es ${usersArray[0]}</div>`;
+            }
         }
+
     })
 }
