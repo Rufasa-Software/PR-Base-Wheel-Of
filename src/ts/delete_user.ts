21d3904d8f743054be57dfc4f5381bfa277afa96
irina-p-d <irinapetrovap@gmail.com>
2021-11-18 21:58:48 +0100

    prueba

--- src/ts/delete_user.ts
+++ src/ts/delete_user.ts
@@ -1,20 +1,19 @@
 
+import { renderList } from "./create_user";
 import { listUsers, usersArray } from "./elements";
 
 export function deleteUser() : void
 {
     const btnDeleteUser = document.querySelectorAll('.btn-delete');
    btnDeleteUser.forEach(btn => {
-       btn.addEventListener ('click', (e)=> {
-           const elementList = document.getElementById(`player-${e.target.id}`);
-           const coinList = document.getElementById(`coin-${e.target.id}`);
+       btn.addEventListener ('click', e => {
+           let idStrIc = (<HTMLElement>e.target).id;
+           
+           const elementList = document.getElementById(`player-${idStrIc}`);
+           const coinList = document.getElementById(`coin-${idStrIc}`);
            elementList.remove();
            coinList.remove();
-           usersArray.forEach(user =>{
-              if (user = elementList) {
-                  usersArray.shift(user);
-              }
-           })
        })
-   });
+     
+    })
 }
--- src/ts/play_event.ts
+++ src/ts/play_event.ts
@@ -1,37 +1,48 @@
 
 import { renderList } from "./create_user";
 import { deleteUser } from "./delete_user";
-import {animationCoin, btnDeleteUser, btnPlay, messageContainer, usersArray } from "./elements";
+import {animationCoin, btnPlay, messageContainer, usersArray } from "./elements";
 
 export function play() : void
 {
-
-    btnPlay.addEventListener('click',() => {
-            let num=Math.floor(Math.random()*usersArray.length);
-            let looser=usersArray[num];
-            console.log(num);
+        btnPlay.addEventListener('click',() => {
+            if (usersArray.length >= 2) {
+                let num=Math.floor(Math.random()*usersArray.length);
+                let looser=usersArray[num];
+                console.log(num);
            
-            for (let i:number = 0; i<animationCoin.length; i++) {
-            animationCoin[i].classList.add("rotate");
-            }
-            
-            setTimeout(() => {
-                usersArray.splice(num,1);
-                if (usersArray.length < 2) {
-                    messageContainer.innerHTML = `<div><p>Por favor, introduce al menos 2 jugadores para empezar el juego</p></div>`
+                for (let i:number = 0; i<animationCoin.length; i++) {
+                animationCoin[i].classList.add("rotate");
                 }
-                if (usersArray.length >= 2) {
-                    messageContainer.innerHTML=`<div><p>El jugador ${looser} ha sido eliminado</p><p class="text-indicator">Pulsa PLAY para seguir jugando</p></div>`;
-                }
-                if (usersArray.length == 1)
-                {          
-                messageContainer.innerHTML=`<div><p>El ganador es ${usersArray[0]}</p></div>`
-            }  
+                
+                setTimeout(() => {
+                    usersArray.splice(num,1);
+                    if (usersArray.length >= 2) {
+                        messageContainer.innerHTML=`<div><p>El jugador ${looser} ha sido eliminado</p><p class="text-indicator">Pulsa PLAY para seguir jugando</p></div>`;
+                    }
+                    if (usersArray.length == 1)
+                    {          
+                    messageContainer.innerHTML=`<div><p>El ganador es ${usersArray[0]}</p></div>`
+                }  
             
             renderList();
             }, 5000);
             messageContainer.innerHTML=`<div></div>`;
+            }
+            else if (usersArray.length < 2){
+                messageContainer.innerHTML = `<div><p>Por favor, introduce al menos 2 jugadores para empezar el juego</p></div>`
+            }
+                
+            
+
+            
 });
+    
+    
+       
+    
+
+   
         
             /*let i:number=0;
             let rotate = animationCoin[i];
